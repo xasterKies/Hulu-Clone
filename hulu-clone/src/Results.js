c9c004b6d38212733a497240ae5db9ab8b062fe6
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-01 10:11:25 +0100

    r

--- hulu-clone/src/Results.js
+++ hulu-clone/src/Results.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Results.css'
 import VideoCard from './VideoCard'
 import axios from  './axios'
