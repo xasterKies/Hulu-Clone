41a19d48b88511cc8e85889cd84ab475112b09fc
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-01 05:36:40 +0100

    h

--- hulu-clone/src/Header.js
+++ hulu-clone/src/Header.js
@@ -5,8 +5,9 @@ function Header() {
     return (
         <div className="header">
             <div className="header__icons">
-                
+
             </div>
+            <img src="https://press:hulu.com/wp.content/uploads/2020/02/hulu-white.png" alt="logo"/>
         </div>
     )
 }
