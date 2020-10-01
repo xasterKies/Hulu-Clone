370dc23cdc6a367c255f5b0bc05a0c5071f1d245
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-01 07:35:03 +0100

    a

--- hulu-clone/src/App.js
+++ hulu-clone/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Nav from './Nav';
+import Results from './Results'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       
       <Header/>
       <Nav/>
+      <Results/>
     </div>
   );
 }
