cfa43fff2e1a116831117a2a4e9f9f8708d50895
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-01 10:52:58 +0100

    v

--- hulu-clone/src/VideoCard.js
+++ hulu-clone/src/VideoCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './VideoCard.css'
+import TextTruncate from "react-text-truncate"
 
 const base_url = "http://image.tmdb.org/t/p/original";
 
@@ -7,7 +8,7 @@ function VideoCard({ movie }) {
     return (
         <div className="videoCard">
             <img src={`${base_url}${movie.backdrop_path || movie.poster_path}`} alt=""/>
-            <p>{movie.release_date || movie.first_air_date}</p>
+            <p>{movie.overview}</p>
             <h2>Movie title</h2>
 
             <p>Number of likes...</p>
