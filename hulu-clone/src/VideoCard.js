bf209c0212bddf1efc6980d23e91d8a7376dbc55
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-01 13:09:18 +0100

    vi

--- hulu-clone/src/VideoCard.js
+++ hulu-clone/src/VideoCard.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import './VideoCard.css'
 import TextTruncate from "react-text-truncate"
 import ThumbUpSharp from '@material-ui/icons/ThumbUpSharp'
 
 const base_url = "http://image.tmdb.org/t/p/original";
 
-const VideoCard = ({ movie }) {
+const VideoCard = forwardRef(({ movie }, ref) => {
     return (
-        <div className="videoCard">
+        <div ref={ref} className="videoCard">
             <img src={`${base_url}${movie.backdrop_path || movie.poster_path}`} alt=""/>
             <TextTruncate
               line={1}
@@ -25,6 +25,6 @@ const VideoCard = ({ movie }) {
             </p>
         </div>
     )
-}
+})
 
 export default VideoCard
