bdae599c382be6e409e0c2082e6a0c7f4d098880
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-01 09:00:28 +0100

    r

--- hulu-clone/src/request.js
+++ hulu-clone/src/request.js
@@ -3,4 +3,5 @@ const API_KEY = '010cf02106be78cd97145ccefed2ec7d';
 export default {
     fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchTopRated:`/movie/top_rated/week?api_key=${API_KEY}&language=en-US`,
+    fetchTrending:`/discover/movie?api_key=${API_KEY}&language=en-US`,
 };
