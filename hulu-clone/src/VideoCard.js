7cf19676f33ee71e72cff41048ddce39e0d8bcb4
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-01 07:27:56 +0100

    vc

--- hulu-clone/src/VideoCard.js
+++ hulu-clone/src/VideoCard.js
@@ -4,7 +4,9 @@ import './VideoCard.css'
 function VideoCard({ movie }) {
     return (
         <div className="videoCard">
-            
+            <img src="" alt=""/>
+            <h2>Movie title</h2>
+            <p>Number of likes...</p>
         </div>
     )
 }
