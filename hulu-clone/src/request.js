2c4f98feb540022a470ce4f8501d9f9e03771ffb
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-01 09:20:16 +0100

    request

--- hulu-clone/src/request.js
+++ hulu-clone/src/request.js
@@ -4,5 +4,12 @@ export default {
     fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchTopRated:`/movie/top_rated/week?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=878`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=37`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=16`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10770`,
+
 };
