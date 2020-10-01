95fe326133c25ee2d89bdf4c605268808c742c8a
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-01 16:37:48 +0100

    Updates

---
+++ precache-manifest.63627b78a8d6411a3848fd3f33792b27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ed8b3aad8a95d21413632b65fa9ce8",
+    "url": "/Hulu-Clone/index.html"
+  },
+  {
+    "revision": "bf4cfe4eac67104363ba",
+    "url": "/Hulu-Clone/static/css/main.0c9c2984.chunk.css"
+  },
+  {
+    "revision": "d96ab35da21238289ab0",
+    "url": "/Hulu-Clone/static/js/2.45a415a6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Hulu-Clone/static/js/2.45a415a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf4cfe4eac67104363ba",
+    "url": "/Hulu-Clone/static/js/main.98b5f4ba.chunk.js"
+  },
+  {
+    "revision": "7f17e45edea30b0b0e0c",
+    "url": "/Hulu-Clone/static/js/runtime-main.9ee240a6.js"
+  }
+]);
