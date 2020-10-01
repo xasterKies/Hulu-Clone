bc979f110f4a4dbe50d3bcbc134d13b50cd3f46c
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-01 10:09:17 +0100

    v

--- hulu-clone/src/VideoCard.js
+++ hulu-clone/src/VideoCard.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './VideoCard.css'
 
-const base_url = "http://image.tmdb.org/t/p/original"
+const base_url = "http://image.tmdb.org/t/p/original";
 
 function VideoCard({ movie }) {
     return (
         <div className="videoCard">
-            <img src={} alt=""/>
+            <img src={`${base_url}${movie.backdrop_path}`} alt=""/>
             <p>This is a film about coding</p>
             <h2>Movie title</h2>
 
