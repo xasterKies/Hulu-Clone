1755535a8b5f58328c7e44c8bd4001d5a6c2225b
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-01 10:45:51 +0100

    v

--- hulu-clone/src/VideoCard.js
+++ hulu-clone/src/VideoCard.js
@@ -6,7 +6,7 @@ const base_url = "http://image.tmdb.org/t/p/original";
 function VideoCard({ movie }) {
     return (
         <div className="videoCard">
-            <img src={`${base_url}${movie.backdrop_path}`} alt=""/>
+            <img src={`${base_url}${movie.backdrop_path || movie.poster_path}`} alt=""/>
             <p>This is a film about coding</p>
             <h2>Movie title</h2>
 
