7c6e97e84716e514c737d484f03bee632c6d98ad
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-01 11:49:41 +0100

    r

--- hulu-clone/src/Results.js
+++ hulu-clone/src/Results.js
@@ -4,7 +4,7 @@ import VideoCard from './VideoCard'
 import axios from  './axios'
 import requests from './requests'
 
-function Results() {
+function Results( {selectedOption}) {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
