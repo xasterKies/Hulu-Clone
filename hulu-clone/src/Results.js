36f2e725a23d6884d84e0bc5a825faa53bdbb463
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-01 09:59:53 +0100

    r

--- hulu-clone/src/Results.js
+++ hulu-clone/src/Results.js
@@ -2,15 +2,16 @@ import React, { useState } from 'react'
 import './Results.css'
 import VideoCard from './VideoCard'
 import axios from  './axios'
-import request from './request'
+import requests from './requests'
 
 function Results() {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
         async function fetchData() {
-            const request = await axios.get(request.
-                )
+            const request = await axios.get(requests.fetchActionMovies)
+            setMovies(request.data.results);
+            return request;
         }
 
         fetchData();
