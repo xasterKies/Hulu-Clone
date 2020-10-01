8396d573323feb4a151759ae579711a28f872de9
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-01 07:18:47 +0100

    video

---
+++ hulu-clone/src/VideoCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './VideoCard'
+
+function VideoCard() {
+    return (
+        <div className="videoCard">
+            
+        </div>
+    )
+}
+
+export default VideoCard
