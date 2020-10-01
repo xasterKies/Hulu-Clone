5c23c4667008429adfe7419afdd5bfe5aa996dfc
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-01 12:08:09 +0100

    n

--- hulu-clone/src/Nav.js
+++ hulu-clone/src/Nav.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import './Nav.css'
+import requests from "./requests";
 
 function Nav({setSelectedOption}) {
     return (
         <div className="nav">
-            <h2>Trending</h2>
-            <h2>Top Rated</h2>
-            <h2>Action</h2>
-            <h2>Horror</h2>
-            <h2>Romance</h2>
-            <h2>Mystery</h2>
+            <h2 onClick={() => setSelectedOption(requests.fetchTrending)}>Trending</h2>
+            <h2 onClick={() => setSelectedOption(requests.fetchTopRated)}>Top Rated</h2>
+            <h2 onClick={() => setSelectedOption(requests.fetchActionMovies)}>Action</h2>
+            <h2 onClick={() => setSelectedOption(requests.fetchHorrorMovies)}>Horror</h2>
+            <h2 onClick={() => setSelectedOption(requests.fetchRomanceMovies)}>Romance</h2>
+            <h2 onClick={() => setSelectedOption(requests.fetchMysteryMovies)}>Mystery</h2>
             <h2>Sci-fi</h2>
             <h2>Werstern</h2>
             <h2>Animation</h2>
