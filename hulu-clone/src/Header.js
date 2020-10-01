cacbcba29282dc55813a33f2c583c20cf3d2c3bf
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-01 05:40:18 +0100

    icon

--- hulu-clone/src/Header.js
+++ hulu-clone/src/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Header.css'
+import HomeIcon from '@material-ui/icons/Home'
 
 function Header() {
     return (
         <div className="header">
             <div className="header__icons">
-                
+                <HomeIcon/>
+
             </div>
             <img src="https://press:hulu.com/wp.content/uploads/2020/02/hulu-white.png" alt="logo"/>
         </div>
